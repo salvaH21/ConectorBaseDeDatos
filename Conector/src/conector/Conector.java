219c9b7df78a92ce8857892162c7d576f7012538
salvaH21 <sholivares90@gmail.com>
2023-11-21 19:42:08 +0100

    Conexión realizada

--- Conector/src/conector/Conector.java
+++ Conector/src/conector/Conector.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package conector;
 
-/**
- *
- * @author salva
- */
+import java.sql.*;
+
 public class Conector {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String url = "jdbc:mysql://localhost:3306/prueba_conector";
+        String usuario = "pruebaconector";
+        String contrasena = "pruebaconector";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection miconexion = DriverManager.getConnection(url, usuario, contrasena);
+            Statement peticion = miconexion.createStatement();
+            String cadena = "SELECT * FROM personas";
+            ResultSet resultado = peticion.executeQuery(cadena);
+            while (resultado.next()) {
+                String nombre = resultado.getString("nombre");
+                String apellidos = resultado.getString("apellidos");
+                String email = resultado.getString("email");
+                System.out.println(nombre + " " + apellidos + " " + email);
+            }
+            resultado.close();
+            peticion.close();
+            miconexion.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
+
 }
